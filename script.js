9a18daace58e8a26f401f96e9e2389e223de649c
mr283746 <96629653+mr283746@users.noreply.github.com>
2021-12-26 14:58:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function bar()
+{
+    document.getElementById('nav').style.width="100%";
+    document.getElementById('nav_bg').style.width="100%";
+    document.getElementById('b1').style.display="none";
+    document.getElementById('b2').style.display="block";
+    document.getElementById('nav_ul').style.display="block";
+}
+function bar2()
+{
+    document.getElementById('nav').style.width="0";
+    document.getElementById('b1').style.display="block";
+    document.getElementById('b2').style.display="none";
+    document.getElementById('nav_bg').style.width="0";
+}
+function li()
+{
+    document.getElementById('nav').style.width="0";
+    document.getElementById('b1').style.display="block";
+    document.getElementById('b2').style.display="none";
+    document.getElementById('nav_bg').style.width="0";
+}
